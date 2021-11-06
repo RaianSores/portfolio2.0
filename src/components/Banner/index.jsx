915210db7e5e479269a77070b8d4b4334da8ae70
Raian Soares <raiansoares72@gmail.com>
2021-11-06 11:33:54 -0300

    Ajuste no Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,17 +1,31 @@
-import { Bio } from "../Bio";
+import { useEffect } from "react";
 import styles from "./styles.module.scss";
 
 export const Banner = () => {
+  useEffect(() => {
+    const typeWrite = (elemento) => {
+      const textoArray = elemento.innerHTML.split("");
+      elemento.innerHTML = " ";
+      textoArray.forEach(function (letra, i) {
+        setTimeout(function () {
+          elemento.innerHTML += letra;
+        }, 90 * i);
+      });
+    };
+
+    const titulo = document.getElementById("titleBio");
+    typeWrite(titulo);
+  }, []);
   return (
-    
     <div className={styles.containerWrapper}>
       <div className={styles.imgWrapper}>
+        <h1 id="titleBio">
+          Sou um dev apaixonado pela inumeras possibilidades que as tecnologias
+          pode proporciar, por meio dela é possível criar coisas incriveis e
+          transformar ideias em soluções!
+        </h1>
         <img src="/bg.jpg" alt="" />
-
       </div>
-      <div className={styles.bioText}>
-          <Bio />
-        </div>
     </div>
   );
 };
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,6 +1,5 @@
 import { Navigation } from "../../components/Navigation";
-import { Bio } from "../../components/Bio";
-import { Profile } from "../../components/Profile";
+import { Banner } from "../../components/Banner";
 import { Skills } from "../../components/Skills";
 import { Menu } from "../../components/Menu";
 import { Projects } from "../../components/Projects";
@@ -14,13 +13,8 @@ export const Home = () => {
     <>
       <div className={styles.containerHome}>
         <Navigation />
-        <div className={styles.left}>
-          <Bio />
-          <Profile />
-        </div>
-        <div className={styles.right}>
-          <Menu />
-        </div>
+        <Banner />
+        <Menu />
         <Skills />
         <Projects />
         <Contact />
